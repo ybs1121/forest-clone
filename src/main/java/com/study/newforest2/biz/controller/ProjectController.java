5de4ee19a31074fbae2e73a60844f24b581fcf10
ybs991 <ybs991@naver.com>
2024-06-06 14:14:21 +0900

    QueryDsl 연습 String to Date 테스트 및 확인 필요

--- src/main/java/com/study/newforest2/biz/controller/ProjectController.java
+++ src/main/java/com/study/newforest2/biz/controller/ProjectController.java
@@ -2,7 +2,9 @@ package com.study.newforest2.biz.controller;
 
 import com.study.newforest2.biz.dto.MemberDto;
 import com.study.newforest2.biz.dto.ProjectDto;
+import com.study.newforest2.biz.dto.ProjectFind;
 import com.study.newforest2.biz.service.ProjectService;
+import com.study.newforest2.core.common.StandardResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +31,12 @@ public class ProjectController {
     @GetMapping("/member")
     public List<MemberDto> getMembers(long id) {
         return projectService.getMemberList(id);
+    }
+
+
+    @GetMapping("/find")
+    public StandardResponse<List<ProjectDto>> getProjects(ProjectFind projectFind) {
+        return StandardResponse.success(projectService.getProjectList(projectFind));
     }
 
 }
